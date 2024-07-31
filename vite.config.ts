41b2291aa4a0aaaa6e3da32538f36c0e0a5afdf5
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2024-07-31 21:35:55 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import vercel from "vite-plugin-vercel";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [react()],
-  // base: "/",
-  // server: {
-  //   open: "/",
-  // },
+  plugins: [react(), vercel()],
 });
