51ead4c150da7a99ca9e6f47788f8fc5d18552ee
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2024-07-31 18:02:29 +0300

    vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";
 import vercel from "vite-plugin-vercel";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vercel()],
-  base: "/kanban-board",
+  plugins: [react(), vercel()],
+  base: "/kanban-board/",
   server: {
-    open: "/kanban-board",
+    open: "/kanban-board/",
     port: 3000,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
   },
 });
